eb4342351307f9b5e2f24c48b76c3de910e273e1
mohad12211 <mohammedkaabi64@gmail.com>
2024-03-25 18:51:17 +0300

    Use MemoryRouter instead of BrowserRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Routes, Route, MemoryRouter } from "react-router-dom"
 import Home from "./components/Home"
 import ChatPage from "./components/ChatPage";
 import socketIO from "socket.io-client"
@@ -8,14 +8,14 @@ const url = process.env.NODE_ENV === 'production' ? "https://typological.me" : "
 const socket = socketIO.connect(url)
 function App() {
   return (
-    <BrowserRouter>
+    <MemoryRouter>
       <div>
         <Routes>
           <Route path="/" element={<Home socket={socket} />}></Route>
           <Route path="/chat" element={<ChatPage socket={socket} />}></Route>
         </Routes>
       </div>
-    </BrowserRouter>
+    </MemoryRouter>
 
   );
 }
